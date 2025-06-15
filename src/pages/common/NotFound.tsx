c363d96d70e5ad548d24512947399c19e0bdf386
kimori-chick <h.mori.tmahr@gmail.com>
2025-06-16 00:04:54 +0900

    add 不正なURL検出時の404画面を追加

---
+++ src/pages/common/NotFound.tsx
@@ -0,0 +1,10 @@
+const NotFound = () => {
+  return (
+    <div style={{ padding: "2rem", textAlign: "center" }}>
+      <h1>404 - ページが見つかりません</h1>
+      <p>お探しのページは存在しないか、URLが間違っています。</p>
+    </div>
+  );
+};
+
+export default NotFound;
--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,6 +1,12 @@
 import { RouteObject } from 'react-router-dom';
 import { signupRoutes } from './signup';
+import NotFound from '../pages/common/NotFound';
 
 export const appRoutes: RouteObject[] = [
   ...signupRoutes,
+
+  {
+    path: '*',
+    element: <NotFound />,
+  },
 ];
