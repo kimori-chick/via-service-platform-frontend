33c2d343b19d21590fd4bca738d8ef8b58ba1cf7
kimori-chick <h.mori.tmahr@gmail.com>
2025-06-16 00:22:36 +0900

    add: メンテナンス時の表示ルート制御を追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import { useRoutes } from 'react-router-dom';
 import { appRoutes } from './router/routes';
+import Maintenance from './pages/common/Maintenance';
+
+// メンテナンス中はこの値を true に変更する
+const isMaintenance = false;
 
 const App = () => {
   const routing = useRoutes(appRoutes);
+
+  if (isMaintenance) {
+    return <Maintenance />;
+  }
+
   return routing;
 };
 
